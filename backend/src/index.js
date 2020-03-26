317e2b92b308bcae16933ea2be41630048f593dd
Igo Jeferson Barros Coelho <igojeferson.coelho@gmail.com>
2020-03-26 19:27:39 -0300

    - Rota de Login, paginação da consulta de incidentes (com join nas ongs) e consulta de incidentes filtrado por ong

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes)
 
@@ -37,7 +39,5 @@ app.use(routes)
  * Driver: SELECT * FROM users
  * Query Builder: table('users').select('*').where()
  */
-
-
 
 app.listen(3333)
